dea1b9f344eca411aad98606369ecaeb2d5e1f62
marti3 <marcioch.s.m@gmail.com>
2025-05-30 02:24:22 +0200

    Add visualization functions and refactor visualization.py for clarity

--- visualisation.py
+++ visualisation.py
@@ -3,21 +3,26 @@ import numpy as np
 import scipy
 from scipy.io import wavfile
 
-print("numpy version:", np.__version__)
-print("scipy version:", scipy.__version__)
 
-samplerate, data = wavfile.read('results\\air\\Fast_Slime_fast_2025-03-07_21.35.05.processed_segment1.wav')
-print(f'Sample rate: {samplerate} Hz')
-print(f'Data shape: {data.shape}')
+def audio_plot(data, samplerate):
+    if len(data.shape) > 1:
+        data = data[:, 0]
 
-if len(data.shape) > 1:
-    data = data[:, 0]
+    times = np.arange(len(data)) / samplerate 
 
-times = np.arange(len(data)) / samplerate 
+    plt.figure(figsize=(12, 4))
+    plt.plot(times, data)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.title('Audio signal in the time domain')
+    plt.show()
 
-plt.figure(figsize=(12, 4))
-plt.plot(times, data)
-plt.xlabel('Czas [s]')
-plt.ylabel('Amplituda')
-plt.title('Sygnał audio w dziedzinie czasu')
-plt.show()
+
+def plot_umap(X_2d, labels):
+    plt.figure(figsize=(8, 6))
+    for label in np.unique(labels):
+        idx = labels == label
+        plt.scatter(X_2d[idx, 0], X_2d[idx, 1], label=label)
+    plt.legend()
+    plt.title("UMAP Projection")
+    plt.show()
