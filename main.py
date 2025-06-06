85d6ecada262d4b92c6c2a83df69937e7ca0eadf
marti3 <marcioch.s.m@gmail.com>
2025-06-06 19:34:21 +0200

    Rename of UMAP function + features scaling

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from feature_extraction import get_all_features
-from classification import reduce_dimensionality, fit_classifier, evaluate_classifier
+from classification import reduce_dimensionality_UMAP, fit_classifier, evaluate_classifier
 from visualisation import plot_umap
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == "__main__":
-    features, labels = get_all_features("results_denoised")
-    X_2d = reduce_dimensionality(features)
+    features_raw, labels = get_all_features("results_denoised")
+    scaler = StandardScaler()
+    features = scaler.fit_transform(features_raw)
+
+    X_2d = reduce_dimensionality_UMAP(features)
     plot_umap(X_2d, labels)
+    
     X_train, X_test, y_train, y_test = train_test_split(X_2d, labels, test_size=0.3, random_state=42)
     clf = fit_classifier(X_train, y_train)
     evaluate_classifier(clf, X_test, y_test)
